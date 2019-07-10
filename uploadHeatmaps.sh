e50bd15af3596e4c615839434b9247d99ead167e
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2019-07-10 16:38:41 -0400

    fixed option parsing

--- uploadHeatmaps.sh
+++ uploadHeatmaps.sh
@@ -31,57 +31,73 @@ brief=true
 while [ -n "$1" ]
 # while loop starts
 do
-case "$1" in
--q) QHOST="$2"
-    shift;;
--h) HOST="$2"
-    shift;;
--P) port="$2"
-    shift ;;
-
--p) passw="$2"
-    shift ;;
-
--c) collection="$2"
-    shift ;;
-
--u) uname="$2"
-    shift ;;
-
--i) in="$2"
-    shift ;;
-
--o) out=${2}
-    shift;;
-
--d) database=${2}
-    shift;;
-
---help)  
-    usage false
-    exit 0;;
-
-*) usage true;; 
- 
-esac
- 
-shift
- 
+  # Process commandline flags
+  case "$1" in
+    -q) qhost="$2"
+        shift;;
+    -h) host="$2"
+        shift;;
+    -P) port="$2"
+        shift ;;
+    -p) passw="$2"
+        shift ;;
+    -c) collection="$2"
+        shift ;;
+    -u) uname="$2"
+        shift ;;
+    -i) in="$2"
+        shift ;;
+    -o) out=${2}
+        shift;;
+    -d) database=${2}
+        shift;;
+    --help)  
+        usage false
+        exit 0;;
+    *) usage true;; 
+  esac
+  shift
 done
 
-if [ -z "${QHOST}" ] || [ -z "${in}" ] || [ -z "${passw}" ] || [ -z "${uname}" ] 
+if [ -z "${collection}" ] || [ -z "${passw}" ] || [ -z "${uname}" ] 
 then
   echo "Missing required parameters"
   usage true
   exit 1
 fi
 
+# Set default values for unprovided options.
+if [ -z "${host}" ]
+then
+  host="ca-mongo"
+fi
+if [ -z "${qhost}" ]
+then
+  qhost="quip-pathdb"
+fi
+if [ -z "${in}" ]
+then
+  in="/data/xfer/input"
+fi
+if [ -z "${out}" ]
+then
+  out="data/xfer/output"
+fi
+if [ -z "${port}" ]
+then
+  port="27017"
+fi
+if [ -z "${database}" ]
+then
+  database="camic"
+fi
+
 # Convert heatmap data in the 'in' folder into uploadable json in the 'out' folder.
-node /usr/local/bin/convert_heatmaps.js -h ${QHOST} -c ${collection} -i ${in} -o ${out} -u ${uname} -p ${passw}
+node /usr/local/bin/convert_heatmaps.js -h ${qhost} -c ${collection} -i ${in} -o ${out} -u ${uname} -p ${passw}
 
 # Import into the quip database
 for filename in ${out}/*.json ; do
-  mongoimport --port ${port} --host ${HOST} -d ${database} -c heatmap ${filename}
+  mongoimport --port ${port} --host ${host} -d ${database} -c heatmap ${filename}
 done
 
 exit 0
